83d571b67356f51cddeb541d751d187c60367394
polipopi <pmbastid@espol.edu.ec>
2024-01-14 13:54:24 -0500

    Imagen de bienvenido

--- Proyecto2P_Grupo2/src/main/java/com/mycompany/proyecto2p_grupo2/BienvenidosController.java
+++ Proyecto2P_Grupo2/src/main/java/com/mycompany/proyecto2p_grupo2/BienvenidosController.java
@@ -4,12 +4,16 @@
  */
 package com.mycompany.proyecto2p_grupo2;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 /**
@@ -24,6 +28,12 @@ public class BienvenidosController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        try(FileInputStream input = new FileInputStream("src/main/resources/images/imgBienvenido.png")){
+            Image i = new Image(input,578,369,false,false);
+            imgBienvenido.setImage(i);
+        }catch(IOException e){
+            System.out.println("No se encuentra el archivo");
+        }
         // TODO
     }    
     
@@ -35,4 +45,6 @@ public class BienvenidosController implements Initializable {
     
     @FXML
     public Button btnReserva;
+    @FXML
+    ImageView imgBienvenido;
 }
